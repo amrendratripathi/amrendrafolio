89f5aa6e5aeede17e6a75153e5286af213c81ce0
amrendratripathi <rajjubabu531@gmail.com>
2026-01-13 23:08:59 +0530

    /

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import TargetCursor from "@/components/TargetCursor";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <TargetCursor targetSelector="button, a[href], .cursor-target" />
       <Toaster />
       <Sonner />
       <BrowserRouter>
